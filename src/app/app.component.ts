8755f5e1dc0c4c5677a3460564e459816b3f256a
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-12-04 19:32:42 -0300

    feat: config app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FloatingContactComponent } from './components/floating-contact/floating-contact.component';
+import { FooterSectionComponent } from './components/footer-section/footer-section.component';
+import { FormationSectionComponent } from './components/formation-section/formation-section.component';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { ServicesSectionComponent } from './components/services-section/services-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    HeroSectionComponent,
+    ServicesSectionComponent,
+    FooterSectionComponent,
+    FloatingContactComponent,
+    FormationSectionComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'civil-engineering-website';
+  title = 'Élio Rodrigues | Engenharia';
 }
