8002af1ffec02a3433f367f00f9abc0aab938149
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-12-06 08:01:47 -0300

    feat: implement observable to toggle mobile menu

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { NAVBAR_LIST } from '../../constants/navbar-list';
 import { NavbarItemComponent } from '../navbar-item/navbar-item.component';
 import { SvgIconComponent } from '../svg-icon/svg-icon.component';
@@ -7,10 +9,16 @@ import { SvgIconComponent } from '../svg-icon/svg-icon.component';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [NavbarItemComponent, RouterModule, SvgIconComponent],
+  imports: [NavbarItemComponent, RouterModule, SvgIconComponent, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
   navbarList = NAVBAR_LIST;
+  showMenuObject = new BehaviorSubject<boolean>(false);
+  isShowMenu$: Observable<boolean> = this.showMenuObject.asObservable();
+
+  onToggleMenu(): void {
+    this.showMenuObject.next(!this.showMenuObject.value);
+  }
 }
