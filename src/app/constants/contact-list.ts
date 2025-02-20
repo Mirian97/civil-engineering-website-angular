da4671dcf4902dada3f8799cffa51648f80f42da
Mirian Quispe <mirianquispe1505@gmail.com>
2025-02-20 19:32:06 -0300

    update zap link

--- src/app/constants/contact-list.ts
+++ src/app/constants/contact-list.ts
@@ -1,5 +1,8 @@
 export const CONTACT_LIST_FOOTER = [
-  { icon: 'phone', link: 'https://w.app/zqyZ3O' },
+  {
+    icon: 'phone',
+    link: 'https://wa.me/5511988343235?text=Ol%C3%A1,%20gostaria%20de%20conversar%20sobre%20um%20servi%C3%A7o...',
+  },
   {
     icon: 'email',
     link: 'https://mail.google.com/mail/?view=cm&fs=1&to=eliorodrigues68@gmail.com',
@@ -25,6 +28,6 @@ export const CONTACT_LIST_FLOATING = [
   },
   {
     icon: 'whatsapp',
-    link: 'https://w.app/zqyZ3O',
+    link: 'https://wa.me/5511988343235?text=Ol%C3%A1,%20gostaria%20de%20conversar%20sobre%20um%20servi%C3%A7o...',
   },
 ];
