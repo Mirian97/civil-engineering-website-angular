1d8a580639fe496be8016779637bc9c16a651af1
Mirian Quispe <mirianquispe1505@gmail.com>
2025-07-12 19:45:09 -0300

    feat: enhance hero section with new layout, styles, and scroll functionality

--- src/app/components/hero-section/hero-section.component.ts
+++ src/app/components/hero-section/hero-section.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../button/button.component';
 import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-hero-section',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, ButtonComponent],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.scss',
 })
-export class HeroSectionComponent {}
+export class HeroSectionComponent {
+  scrollToServices(): void {
+    const servicesSection = document.getElementById('servicos');
+    if (servicesSection) {
+      servicesSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+}
